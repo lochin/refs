e501e4e22fa94ccfb1c982b8a977ea2bd97fc2ae
Erkaboy-coder <erkaboy756.5313@gmail.com>
2024-11-06 18:11:16 +0500

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); // Install Express via npm if you haven't
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
